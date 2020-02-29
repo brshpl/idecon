2105c7529b5d467be6f43fc9e8f468270d6446f3
Igor <igor_barishpol@mail.ru>
2020-02-29 14:42:44 +0300

    First commit

---
+++ lib/idecon.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'idecon/version'
+
+module Idecon
+  class Error < StandardError; end
+
+  1
+end
---
+++ lib/idecon/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Idecon
+  VERSION = '0.1.0'
+end
