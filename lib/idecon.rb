fb28d352618d5f345e8d41ca881a77b685472cd3
Igor <igor_barishpol@mail.ru>
2020-02-29 16:52:19 +0300

    added method color

--- lib/idecon.rb
+++ lib/idecon.rb
@@ -9,6 +9,7 @@ module Idecon
     def initialize(user_name, path)
       @hash = Digest::MD5.hexdigest(user_name)
       @path = path
+      @color = [0, 0, 0]
     end
 
     def generate
@@ -18,15 +19,28 @@ module Idecon
 
     private
 
+    # Create matrix, where 1 - coloured square, 0 - empty square
     def create_matrix
-      @hash.chars[0..14].map do |c|
+      color
+      @matrix = @hash.chars[0..24].map do |c|
         c = if (0..4).include?(c) || ('a'..'m').include?(c)
-              1
-            else 0
+              @color
+            else [0, 0, 0]
             end
         c
       end
     end
 
+    # Get color from hash
+    def color
+      @hash[24..32].scan(/(.)(.)(.)/).map do |arr|
+        color = 0
+        arr.each_with_index do |c, i|
+          color += 10**i + (c[i].to_i(36) + 10).to_s[1]
+        end
+        color
+      end
+      @color = color
+    end
   end
 end
