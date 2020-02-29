da2c91fce91036e3d357d0b5b1e43a1bc00a512a
Igor <igor_barishpol@mail.ru>
2020-02-29 16:25:23 +0300

    added method create_matrix

--- lib/idecon.rb
+++ lib/idecon.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
 
 require 'idecon/version'
+require 'digest'
 
 module Idecon
   class Error < StandardError; end
+  class Identicon
+    def initialize(user_name, path)
+      @hash = Digest::MD5.hexdigest(user_name)
+      @path = path
+    end
 
-  1
+    def generate
+      create_matrix
+
+    end
+
+    private
+
+    def create_matrix
+      @hash.chars[0..14].map do |c|
+        c = if (0..4).include?(c) || ('a'..'m').include?(c)
+              1
+            else 0
+            end
+        c
+      end
+    end
+
+  end
 end
